1e854b6277145e3b6ad98fb763ff25fd893b601e
Abhinav00010 <abhinav.vibhuthi0001@gmail.com>
2026-08-13 16:35:24 +0530

    changes done

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,26 @@
-alert("Welcome to the Web Bootcamp!");
+//alert("Welcome to the Web Bootcamp!");
+
+var users = [
+  {
+    name: "John Doe",
+    gender: "Male",
+    image: "john.png",
+  },
+  {
+    name: "Jane Smith",
+    gender: "Female",
+    image: "jane.png",
+  },
+];
+
+var curId = 0;
+
+function toggleUser() {
+  curId = (curId + 1) % 2;
+  var userName = document.getElementById("user-name");
+  var userImage = document.getElementById("user-image");
+  var userGender = document.getElementById("user-gender");
+  userName.innerHTML = users[curId].name;
+  userImage.src = users[curId].image;
+  userGender.innerHTML = users[curId].gender;
+}

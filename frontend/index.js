a943b7bc4cc43f7ccf4aeaf337b87f37e605384f
Abhinav00010 <abhinav.vibhuthi0001@gmail.com>
2026-08-14 10:47:58 +0530

    random user function added

--- frontend/index.js
+++ frontend/index.js
@@ -24,3 +24,26 @@ function toggleUser() {
   userImage.src = users[curId].image;
   userGender.innerHTML = users[curId].gender;
 }
+
+function randomUser() {
+  fetch("https://randomuser.me/api")
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      var userName = document.getElementById("user-name");
+      var userImage = document.getElementById("user-image");
+      var userGender = document.getElementById("user-gender");
+
+      var newUserName =
+        data.results[0].name.first + " " + data.results[0].name.last;
+      var newUserGender = data.results[0].gender;
+      var newUserImage = data.results[0].picture.large;
+      userName.innerHTML = newUserName;
+      userImage.src = newUserImage;
+      userGender.innerHTML = newUserGender;
+    })
+    .catch(function (error) {
+      console.log("Error fetching random user:" + error);
+    });
+}
